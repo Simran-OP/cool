08400b3af0ba4e6751b67fa1c51a31b581644dec
simran saini <106807449+TalkIsCheapShowMeCode@users.noreply.github.com>
2023-03-02 11:33:56 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,157 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Climate+Crisis&family=Eczar:wght@800&family=Poppins:wght@100;200;300;500;600;700;900&display=swap" rel="stylesheet">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<link rel="stylesheet" href="style.css">
+    <title>Hello, world!</title>
+  </head>
+  
+  
+  <body>
+    <nav class="navbar fixed-top navbar-expand-sm navbar-light bg-light">
+	<div class="container">
+  <a class="navbar-brand logo" href="#">GOONJ</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav ml-auto">
+      <a class="nav-item nav-link active" href="#home">Home</a>
+      <a class="nav-item nav-link" href="#about">About</a>
+      <a class="nav-item nav-link" href="#Price">Pricing</a>
+      <a class="nav-item nav-link" href="#contacts">contacts</a>
+    
+  </div>
+  </div>
+  <a href="login.php">
+	<button type="button" class="btn btn-light" style="marign-left:90px;" href="login.php">LOGIN</button>
+	</a>
+  </div>
+</nav>
+<script>document.onreadystatechange = function() {
+  let lastScrollPosition = 0;
+  const navbar = document.querySelector('.navbar');
+  window.addEventListener('scroll', function(e) {
+    lastScrollPosition = window.scrollY;
+    
+    if (lastScrollPosition > 100)
+      navbar.classList.add('navbar-dark');
+    else
+      navbar.classList.remove('navbar-dark');
+  });
+}
+</script>
+
+<div class="outer" id="home">
+<div class="inner-box">
+<h2>GOONJ</h2>
+<p>Biggest fest ever<br>come and join us at UIET</p>
+</div>
+</div>
+
+
+
+<!---hero-->
+<div style="margin-top:800px;">
+<div class="container d-flex justify-content-around">
+<div class="about float-left ">
+</div><div class="about-txt float-right"><p>Goonj is the annual cultural festival of UIET, Panjab University, Chandigarh. It is a platform for students to showcase their talents and showcase the cultural diversity of the institution. The festival is eagerly awaited by students and staff alike and is a much-needed break from the rigors of academic life. Goonj is a celebration of art, music, dance, and culture, and it brings together students from different backgrounds and cultures to create a truly vibrant and inclusive environment. The festival is organized by a team of dedicated students and is supported by the faculty and staff of UIET. The festival is an opportunity for students to showcase their talents, network with peers and industry professionals, and create lifelong memories.</p>
+</div>
+</div>
+<div class="gallery">
+<div class="container1">
+  <h1>WELCOME THE STARS</h1>
+
+  <div class="gallery-wrap">
+    <div class="item item-1"></div>
+    <div class="item item-2"></div>
+    <div class="item item-3"></div>
+    <div class="item item-4"></div>
+    <div class="item item-5"></div>
+  </div>
+ </div>
+</div>
+
+
+<div class="social">
+  <a href="https://twitter.com/StefCharle" target="_blank">
+    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/149103/twitter.svg" alt="">
+  </a>
+</div>
+</div>
+
+
+
+ <link href='//netdna.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css' rel='stylesheet' type='text/css'/>  
+
+<footer id='social-footer'>
+        <ul>
+
+            <li class='highlight'>
+                <a class="fa-stack fa-1x" data-toggle="tooltip" title="Email" data-placement="top" href="mailto:@.com">
+                    <i class="fa fa-square fa-stack-2x"></i>
+                    <i class="fa fa-envelope fa-stack-1x blacktext"></i>
+                </a>
+            </li>
+
+            <li class='highlight'>
+                <a class="fa-stack fa-1x" data-toggle="tooltip" title="FreeCodeCamp" data-placement="top" href="https://www.freecodecamp.com/" target="_blank">
+                    <i class="fa fa-square fa-stack-2x"></i>
+                    <i class="fa fa-fire fa-stack-1x blacktext"></i>
+                </a>
+            </li>
+
+            <li class='highlight'>
+                <a class="fa-stack fa-1x" data-toggle="tooltip" title="Linkedin" data-placement="top" href="https://www.linkedin.com/in/" target="_blank"><i class="fa fa-linkedin-square fa-stack-2x"></i></a>
+            </li>
+
+            <li class='highlight'>
+                <a class="fa-stack fa-1x" data-toggle="tooltip" title="Twitter" data-placement="top" href="https://twitter.com/" target="_blank"><i class="fa fa-twitter-square fa-stack-2x"></i></a>
+            </li>
+
+            <li class='highlight'>
+                <a class="fa-stack fa-1x" data-toggle="tooltip" title="GitHub" data-placement="top" href="https://github.com/" target="_blank"><i class="fa fa-github-square fa-stack-2x"></i></a>
+            </li>
+
+            <li class='highlight'>
+                <a class="fa-stack fa-1x" data-toggle="tooltip" title="CodePen" data-placement="top" href="https://codepen.io//" target="_blank">
+                    <i class="fa fa-square fa-stack-2x"></i>
+                    <i class="fa fa-codepen fa-stack-1x blacktext"></i>
+                </a>
+            </li>
+
+            <li class='highlight'>
+                <a class="fa-stack fa-1x" data-toggle="tooltip" title="Facebook" data-placement="top" href="https://www.facebook.com/" target="_blank"><i class="fa fa-facebook-square fa-stack-2x"></i></a>
+            </li>
+
+            <li class='highlight'>
+                <a class="fa-stack fa-1x" data-toggle="tooltip" title="Google+" data-placement="top" href="https://plus.google.com//posts/p/pub?hl=en" target="_blank"><i class="fa fa-google-plus-square fa-stack-2x"></i></a>
+            </li>
+
+        </ul>
+
+
+
+
+
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
